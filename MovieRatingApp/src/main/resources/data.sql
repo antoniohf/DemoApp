e3355ca5b364aa31850891b5c89b6891c9e78442
António Homem Ferreira <antonio.h.ferreira@gmail.com>
2019-11-16 16:05:42 +0000

    fix id on script

--- MovieRatingApp/src/main/resources/data.sql
+++ MovieRatingApp/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 -- password is 'pass'
 insert into users(id,login_name,password,email) values(1,'user','$2a$10$livXCWdzjHEMqprs7l6KPeO/8x15xzdE622TPNvCD8j6ANZuFjfXK','email');
 
-insert into movie(id,title,release_date) values(1,'Commando',CURRENT_DATE)
+insert into movie(id,title,release_date) values(100001,'Commando',CURRENT_DATE)
